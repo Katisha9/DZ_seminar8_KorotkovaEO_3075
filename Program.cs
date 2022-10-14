406227c6e1f6ee65b2c60191b4e48d78a3af8ec6
Ekaterin <katrikacy@mail.ru>
2022-10-14 17:14:37 +0300

    выполение дз 8 обновление

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
-﻿//Урок 8. Как не нужно писать код. Часть 2. Задача 54: Задайте двумерный массив. 
-//Напишите программу, которая упорядочит по убыванию 
-// элементы каждой строки двумерного массива.
+﻿//LВыполнение ДЗ к уроку 8. Как не нужно писать код. Часть 2. 
+
+//Задача 54: Задайте двумерный массив. 
+// Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
 // Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
@@ -10,69 +11,67 @@
 // 9 5 3 2
 // 8 4 4 2
 
-// int[,] array = GetArray(3, 4, 0, 9);
-// PrintArray(array);
-// Console.WriteLine();
+int[,] array = GetArray(3, 4, 0, 9);
+PrintArray(array);
+Console.WriteLine();
 
-// SortRowElements(array);
-// PrintArray(array);
+SortRowElements(array);
+PrintArray(array);
 
-// void SortRowElements(int[,] array)
-// {
-//   for (int i = 0; i < array.GetLength(0); i++)
-//   {
-//     for (int j = 0; j < array.GetLength(1); j++)
-//     {
-//       for (int k = 0; k < array.GetLength(1) - 1; k++)
-//       {
-//         if (array[i, k] < array[i, k + 1])
-//         {
-//           int temp = array[i, k + 1];
-//           array[i, k + 1] = array[i, k];
-//           array[i, k] = temp;
-//         }
-//       }
-//     }
-//   }
-// }
+void SortRowElements(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
 
 
-
-// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая 
-// будет находить строку с наименьшей суммой элементов.
+// Задача 56: Задайте прямоугольный двумерный массив. 
+//Напишите программу, которая будет находить строку с наименьшей суммой элементов.
 // Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
 // 8 4 2 4
 // 5 2 6 7
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки 
-// с наименьшей суммой элементов: 1 строка
+// с наименьшей суммой элементов: 1 строка.
 
+/*
+ int[,] array = GetArray(4, 4, 0, 9);
+ PrintArray(array);
 
-// int[,] array = GetArray(4, 4, 0, 9);
-// PrintArray(array);
+ int MinRowSumm = 37; // 36 - это максимально возможная сумма значений строке с 4 элементами и максимальным возможным значением элементов = 9
+ int MinRowIndex = 0;
 
-// int MinRowSumm = 37;
-// int MinRowIndex = 0;
+ for (int i = 0; i < 4; i++)
+ {
+     int RowSumm = 0;
 
-// for (int i = 0; i < 4; i++)
-// {
-//     int RowSumm = 0;
+     for (int j = 0; j < 4; j++)
+     {
+         RowSumm += array[i, j];
+     }
+     if (RowSumm < MinRowSumm)
+     {
+         MinRowSumm = RowSumm;
+         MinRowIndex = i;
+     }
+ }
 
-//     for (int j = 0; j < 4; j++)
-//     {
-//         RowSumm += array[i, j];
-//     }
-//     if (RowSumm < MinRowSumm)
-//     {
-//         MinRowSumm = RowSumm;
-//         MinRowIndex = i;
-//     }
-// }
-
-// Console.Write($"Номер строки с наименьшей суммой элементов: {MinRowIndex + 1}");
-
-
+ Console.Write($"Номер строки с наименьшей суммой элементов: {MinRowIndex + 1}");
+*/
 
 
 // Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
@@ -82,10 +81,10 @@
 // Результирующая матрица будет:
 // 18 20
 // 15 18
+
 /*
 int[,] arrayFirst = GetArray(2, 2, 0, 9);
 int[,] arraySecond = GetArray(2, 2, 0, 9);
-
 
 PrintArray(arrayFirst);
 Console.WriteLine();
@@ -94,7 +93,6 @@ PrintArray(arraySecond);
 Console.WriteLine();
 
 MultiplyArray(arrayFirst, arraySecond);
-
 
 void MultiplyArray(int[,] arrayFirst, int[,] arraySecond)
 {
@@ -115,7 +113,7 @@ void MultiplyArray(int[,] arrayFirst, int[,] arraySecond)
 }
 */
 
-// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
 //Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 // Массив размером 2 x 2 x 2
 // 66(0,0,0) 25(0,1,0)
@@ -124,7 +122,6 @@ void MultiplyArray(int[,] arrayFirst, int[,] arraySecond)
 // 26(1,0,1) 55(1,1,1)
 
 /*
-
 int[,,] array3dim = new int[2, 2, 2];
 GetArray3dim(array3dim);
 PrintArray3dim(array3dim);
@@ -174,15 +171,12 @@ void PrintArray3dim(int[,,] array3dim)
             for (int j = 0; j < array3dim.GetLength(1); j++)
             {
                 Console.Write($"{array3dim[i, j, k]} ({i}, {j}, {k}) ");
-                            }
-                             Console.WriteLine();
-                            
-                    }
-        Console.WriteLine();
             }
-            
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
 }
-
 */
 
 
@@ -194,7 +188,6 @@ void PrintArray3dim(int[,,] array3dim)
 // 10 09 08 07
 
 /*
-
 int[,] arraySpiral = new int[4, 4];
 
 int temp = 1;
@@ -233,29 +226,38 @@ void WriteArray (int[,] array)
 }
 */
 
-// Рекомендовано решить(задача с семинара)
-// Отсортировать нечетные столбцы массива по возрастанию. Вывести массив изначальный и массив с отсортированными нечетными столбцами
+// Рекомендовано решить
+//Задача с семинара. Отсортировать нечетные столбцы массива по возрастанию. Вывести массив изначальный и массив с отсортированными нечетными столбцами
+/*
 int[,] array = GetArray(3, 5, 0, 9);
 PrintArray(array);
 Console.WriteLine();
 
+ArraySortedColums(array);
+PrintArray(array);
 
-int[,] arraySorted = new int[3,5];
+void ArraySortedColums(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j+=2)
+  {
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+      for (int k = 0; k < array.GetLength(0) - 1; k++)
+      {
+        if (array[k, j] > array[ k + 1, j])
+        {
+          int temp = array[k + 1,j];
+          array[k + 1,j] = array[k,j];
+          array[k,j] = temp;
+        }
+      }
+    }
+  }
+}
+*/
 
 
-
-PrintArray(arraySorted);
-
-
-// Console.WriteLine("Введите количество строк (m): ");
-// int m = int.Parse(Console.ReadLine()!);
-
-// Console.WriteLine("Введите количество столбцов (n): ");
-// int n = int.Parse(Console.ReadLine()!);
-
-// int[,] array = GetArray(m, n, 0, 99);
-// PrintArray(array);
-
+// методы по созданию и заполнению массивов
 int[,] GetArray(int m, int n, int minValue, int maxValue)
 {
     int[,] result = new int[m, n];
